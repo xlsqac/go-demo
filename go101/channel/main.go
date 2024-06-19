b231e89a3b56ec98aa3456e9e6350fe288eb14c8
xlsqac <xlsqac@foxmail.com>
2024-06-19 18:06:09 +0800

    daily

--- go101/channel/main.go
+++ go101/channel/main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
@@ -21,13 +22,31 @@ func main() {
 	//}
 
 	{
-		c3 := make(chan int, 1)
-		fmt.Println(len(c3)) // 0
-		fmt.Println(cap(c3)) // 1
-		c3 <- 1
-		fmt.Println(len(c3)) // 0
-		close(c3)
-		v, sentBeforeClosed := <-c3
-		fmt.Println(v, sentBeforeClosed)
+		//c3 := make(chan int, 1)
+		//fmt.Println(len(c3)) // 0
+		//fmt.Println(cap(c3)) // 1
+		//c3 <- 1
+		//fmt.Println(len(c3)) // 0
+		//close(c3)
+		//v, sentBeforeClosed := <-c3
+		//fmt.Println(v, sentBeforeClosed)
+	}
+	{
+		var ball = make(chan string)
+		kickBall := func(playerName string) {
+			for {
+				fmt.Print(<-ball, "传球", "\n")
+				time.Sleep(time.Second)
+				ball <- playerName
+
+			}
+		}
+		go kickBall("张三")
+		go kickBall("李四")
+		go kickBall("王")
+		go kickBall("刘")
+		ball <- "裁判"    // 开球
+		var c chan bool // 一个零值nil通道
+		<-c             // 永久阻塞在此
 	}
 }
